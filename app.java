ca4aa21ee56fbdbd74124f30181c3323a34a2e6a
root <root@ip-172-31-18-63.ec2.internal>
2026-02-25 10:24:22 +0000

    java code added

--- app.java
+++ app.java
@@ -1,4 +1,10 @@
-public class Main {
+<head>
+<style>
+body {background-color: powderblue;}
+h1 {color: blue;}
+p {color: red;}
+</style>
+</head>public class Main {
    public static void main(String[] args) {
        System.out.println("Hello World");
    }
